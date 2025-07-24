1fb0f6481f3d8cfb2935b69d790030fb872ca69a
Bruno Oliveira <a2019136478@isec.pt>
2025-07-24 19:39:57 +0100

    Simple GET

---
+++ src/main/java/com/FPLBE/FPLBE/FplbeApplication.java
@@ -0,0 +1,13 @@
+package com.FPLBE.FPLBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplbeApplication.class, args);
+	}
+
+}
